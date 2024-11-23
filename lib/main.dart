0da02494d781a0604891c48d3793616950801338
Shaharyar <shaharyar4t4@gmail.com>
2024-11-23 21:52:08 +0500

    updated all files

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medicalapp/firebase_options.dart';
-import 'package:medicalapp/screen/add_card.dart';
-import 'package:medicalapp/screen/dashbored.dart';
-import 'package:medicalapp/screen/onbroad.dart';
-import 'package:medicalapp/screen/product_page.dart';
-import 'package:medicalapp/screen/profile.dart';
-import 'package:medicalapp/screen/signup.dart';
 import 'package:medicalapp/screen/splash_screen.dart';
 
 void main() async{
